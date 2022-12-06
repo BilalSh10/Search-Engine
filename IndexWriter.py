dcb8c59621a3050cecc7f80b7e5236adb5c8eac1
Bilal Shweike <81863952+BilalSh10@users.noreply.github.com>
2022-12-06 16:52:15 +0200

    Update indexWriter.py

--- IndexWriter.py
+++ IndexWriter.py
@@ -1,16 +1,6 @@
 import json
 import os
 import shutil
-# first digit: to count which review we are in, 
-# second digit: to count all the reviews in file, 
-# third digit: number of words in all the reviews. 
-# fourth digit: count how many times the word has been in a single review
-whichReview = [-1, 0, 0, 0]    
-nestedList = [['??????', 1, [0, [['????????', 0]]]]]
-tempId = []
-listOfReviewsId = []
-reviewId = 'id'
-
 
 
 class IndexWriter:
@@ -40,28 +30,22 @@ class IndexWriter:
         productsListFile = os.path.join(path, "products.lst")
 
         with open(self.inputFile,'r+') as readFile:
-            with open(textDicFile, 'w') as writeFile:
-                with open(textListFile, 'w') as listFile:
-
-                    self.recursion(readFile, writeFile, listFile)
-
-                    # write the dictionary and the pointer list to there files.
-                    for list in nestedList:
-                        writeFile.write("%s\n" % list) 
-                    for list in listOfReviewsId:
-                        listFile.write("%s\n" % list)
-                    
-                    with open(collectionDataFile, 'w') as collectionFile:
-                        data = [whichReview[1], whichReview[2]]
-                        collectionFile.write("%s\n" % data)
+            readFile.seek(0)
+            with open(collectionDataFile, 'w') as colleactionFile:
+                self.buildCollectionData(readFile, colleactionFile)
 
             readFile.seek(0)
             with open(reviewTableFile, 'w') as reviewsTableFile:
-                self.buildReviewsTable( readFile, reviewsTableFile)
+                self.buildReviewsTable(readFile, reviewsTableFile)
 
             readFile.seek(0)
             with open(productsDicFile, 'w') as productDicFile:
                 self.buildProductDictionary(readFile, productDicFile, productsListFile)
+
+            readFile.seek(0)
+            with open(textDicFile, 'w') as textDicFile:
+                self.buildTextDictionary(readFile, textDicFile, textListFile)
+
 
     def removed (self, dir):
         """Delete all index files by removing the given
@@ -71,90 +55,6 @@ class IndexWriter:
         shutil.rmtree(path)	
         return 5
 
-
-    # modify the list and write it to the file. 
-    # @param file to write to "text.dic"
-    # @param the splitted list that i wanna write to the file 
-    def WriteToTheFileFunc(self, splittedList):
-
-        checkerList = []
-        
-        for item in splittedList:
-        
-            whichReview[2] = whichReview[2] + 1
-
-            # this will move all non alphabatcal letters from the word.
-            item = item.lower()
-            if item.isalpha() == False:
-                for char in item:
-                    if char.isalnum() == False:
-                        item = item.replace(char, "")
-            
-            # fill the checker list to check if the word is already exist
-            for existWord in nestedList:
-                checkerList.append(existWord[0]) 
-        
-            # if the word is not exist it will be added to dictionary
-            if item not in checkerList:
-                nestedList.append([item, 1, whichReview[2]])         
-                listItem = [whichReview[2], reviewId, 1]
-                listOfReviewsId.append(listItem)
-    
-            # if the word is already exist in the dictionary
-            else:     
-                for smallList in nestedList:
-                    if smallList[0] == item:  
-                        smallList[1] = smallList[1] + 1
-                        for list in listOfReviewsId:
-                            if smallList[2] == list[0]:
-                                # if the word has been reapted in the same review id
-                                if reviewId == list[1]:
-                                    list[2] += 1
-                                    break
-                                # if the word has been reapted in different review id that has been before
-                                else:
-                                    toAddList = [list[0], reviewId, 0]
-                                    toAddList[2] += 1
-                                    listOfReviewsId.append(toAddList)
-                                    break
-                        break
-
-    # in this function i take every line in the file and check if "review/text" apears
-    # in the line and split this line to a list (every word in this list is an item) to write it after that to the text.dic file
-    # @param readfile is the file to read from "100.txt",
-    # @param writefile is the file to write to "text.dic"
-    def recursion(self, readfile, writefile, listFile):
-
-        tempLine = readfile.readline()
-        if len(tempLine) == 0:
-            return
-    
-        word = "review/text"
-        id = "review/userId"
-        global tempId
-        global reviewId
-
-        if id in tempLine:
-            tempId = tempLine.split()     # it will get the review id 
-            tempId.pop(0)
-            reviewId = tempId[0]
-
-        if word in tempLine:   
-
-            whichReview[0] = whichReview[0] + 1
-            whichReview[1] = whichReview[1] + 1
-            splittedList = tempLine.split() 
-            splittedList.pop(0)                         # all the text seperated and put in a list
-            
-            self.WriteToTheFileFunc(splittedList)
-
-            if whichReview[0] == 0:             
-                nestedList.pop(0)
-
-            self.recursion(readfile, writefile, listFile)
-
-        else:
-            self.recursion(readfile, writefile, listFile)
 
 
     def buildReviewsTable(self, readFile, reviewsTableFile):
@@ -223,7 +123,6 @@ class IndexWriter:
         proId = "product/productId"
         id = "review/userId"
         pointerToList = 1
-        numberOfRepetition = 0
         firstMove = 0
         DictionaryForProductsId = {}
         DictionaryProductList = {}
@@ -278,4 +177,117 @@ class IndexWriter:
                 firstMove += 1
 
         productDicFile.write(json.dumps(DictionaryForProductsId, indent=4))
+
+
+
+    def buildTextDictionary(self, readFile, textDicFile, textListFile):
+
+        textId = "review/text"
+        id = "review/userId"
+        pointerToList = 1
+        numberOfRepetition = 1
+        firstMove = 0
+        DictionaryForWords = {}
+        DictionaryWordsList = {}
+        whichReviewId = 0
+        co = 0
+        bo = False
+
+        reviewsList = []
+        for line in readFile:               # add all the reviews in a list called reviewsList
+            if id in line:
+                    tempReviewId = line.split()
+                    tempReviewId.pop(0)
+                    tempReviewId = tempReviewId[0]
+                    reviewsList.append(tempReviewId)
+                    co += 1
+
+        readFile.seek(0)
+        for line in readFile:
+            if line == '\n':               # to know which review we are in
+                whichReviewId += 1
+
+            if textId in line:
+                checkLine = line.split()
+                checkLine.pop(0)
+                while checkLine:
+                    word = checkLine[0].lower()
+                    if not word.isalpha():
+                        for char in word:
+                            if not char.isalnum():
+                                liso = word.replace(char, " ")
+                                liso = liso.split()
+                                if liso:
+                                    word = liso[0]
+                                    for item in liso[1:]:
+                                        checkLine.append(item)
+                                else:
+                                    bo = True
+                    if bo:
+                        checkLine.pop(0)
+                        bo = False
+                        continue
+
+                    if firstMove == 0:
+                        DictionaryForWords[word] = {}                    # build a dic for the spesific product id
+                        DictionaryForWords[word]["word"] = word
+                        DictionaryForWords[word]["pointer_To_List"] = pointerToList
+                        DictionaryForWords[word]["number_of_repetition"] = numberOfRepetition
+                        DictionaryWordsList[pointerToList] = {}
+                        DictionaryWordsList[pointerToList]["Review_Id"] = reviewsList
+                        DictionaryWordsList[pointerToList]["iteration_times"] = [0] * co
+                        DictionaryWordsList[pointerToList]["iteration_times"][whichReviewId]  += 1
+
+                            
+                    keysList = DictionaryForWords.keys()
+
+                    if firstMove != 0:
+
+                        if word not in keysList :
+                            pointerToList += 1
+                            DictionaryForWords[word] = {}              # build a dic for the spesific product id
+                            DictionaryForWords[word]["word"] = word
+                            DictionaryForWords[word]["pointer_To_List"] = pointerToList
+                            DictionaryForWords[word]["number_of_repetition"] = numberOfRepetition
+                            DictionaryWordsList[pointerToList] = {}
+                            DictionaryWordsList[pointerToList]["Review_Id"] = reviewsList
+                            DictionaryWordsList[pointerToList]["iteration_times"] = [0] * co
+                            DictionaryWordsList[pointerToList]["iteration_times"][whichReviewId] += 1
+
+                        else:
+                            for key in DictionaryForWords:
+                                if key == word:
+                                    DictionaryForWords[key]["number_of_repetition"] += 1
+                                    tempPoi = DictionaryForWords[key]["pointer_To_List"]
+                                    DictionaryWordsList[tempPoi]["iteration_times"][whichReviewId] += 1
+                                    break
+                                                    
+                    firstMove += 1
+                    checkLine.pop(0)
+
+        with open(textListFile, "w") as textList0: 
+            textList0.write(json.dumps(DictionaryWordsList, indent=4))
+
+        textDicFile.write(json.dumps(DictionaryForWords, indent=4))
+
+
+
+    def buildCollectionData(self, readFile, colleactionFile):
+        
+        textId = "review/text"
+        numberOfReviews = 0
+        numberOfWords = 0
+
+        for line in readFile:
+            if line == '\n':              
+                numberOfReviews += 1
+
+            if textId in line:
+                checkLine = line.split()
+                checkLine.pop(0)
+                numberOfWords = numberOfWords + len(checkLine)
+
+        collection = [numberOfReviews, numberOfWords]
+        colleactionFile.write(f'{collection}')
+
 
